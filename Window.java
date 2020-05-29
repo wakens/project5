0774f26e5cf6d2bab0092ca0e7a5fe109835da3d
wakens <65771520+wakens@users.noreply.github.com>
2020-05-29 18:57:30 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -2,45 +2,48 @@ import javax.swing.Icon;
 import java.awt.Component;
 import javax.swing.JOptionPane;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
 public class Window
 {
+    /*
+     * This method creates an easy way to display a String message in JOptionPane.
+     */
     public void msg(final String msg) {
         JOptionPane.showMessageDialog(null, msg);
     }
-    
-    public String in(final String msg) {
-        return JOptionPane.showInputDialog(msg);
-    }
-    
-    public static int option(final String[] options, final String msg) {
-        return JOptionPane.showOptionDialog(null, msg, "Click a button", -1, 1, null, options, options[0]);
-    }
-    
-    public void println(final String msg) {
-        System.out.println(msg);
-    }
-    
-    public void print(final String msg) {
-        System.out.println(msg);
-    }
-    
+    /*
+     * This method creates an easy way to display an Object message in JOptionPane.
+     */ 
     public void msg(final Object msg) {
         JOptionPane.showMessageDialog(null, msg);
     }
-    
+ 
+    /*
+     * This method creates an easy way to display a String message
+     * and ask for user input in JOptionPane.
+     */
+    public String in(final String msg) {
+        return JOptionPane.showInputDialog(msg);
+    }
+    /*
+     * This method creates an easy way to display an Object message
+     * and ask for user input in JOptionPane.
+     */
     public String in(final Object msg) {
         return JOptionPane.showInputDialog(msg);
     }
     
-    public int option(final Object[] options, final Object msg) {
+    /*
+     * This method creates an easy way to display a String message
+     * along with buttons that lead to different options.
+     */
+    public int option(final String[] options, final String msg) {
         return JOptionPane.showOptionDialog(null, msg, "Click a button", -1, 1, null, options, options[0]);
     }
-    
-    public String showInputDialog(final String evaluateQuestion) {
-        return null;
+    /*
+     * This method creates an easy way to display an Object message
+     * along with buttons that lead to different options.
+     */
+    public int option(final Object[] options, final Object msg) {
+        return JOptionPane.showOptionDialog(null, msg, "Click a button", -1, 1, null, options, options[0]);
     }
 }
